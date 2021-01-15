765e7cbc6eb855b8f768fe8f890a26b702eb3891
bsfuji <fujimoto@bravesoft.co.jp>
2021-01-15 22:21:00 +0900

    ログインAPIとログアウトAPIを追加。

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout'])->name('logout');
 });
