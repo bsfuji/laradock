c8e0a723eeebdf912a5fad18ae2de54c9b85a757
bsfuji <fujimoto@bravesoft.co.jp>
2021-01-15 22:22:45 +0900

    認証エラー時のレスポンスを変更する。

--- laravel/app/Exceptions/Handler.php
+++ laravel/app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -36,5 +37,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+                    ? response()->json(['code' => 401, 'message' => 'Unauthorized.'], 401)
+                    : redirect()->guest($exception->redirectTo() ?? route('login'));
     }
 }
